b6439f0c6ffcc3ef59acdadfbf0b713fa70355e0
쿠러그 도우미 <we_are@khlug.org>
2026-01-09 16:52:14 +0000

    chore: bump versionCode to 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "org.khlug"
         minSdk = 29
         targetSdk = 34
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
